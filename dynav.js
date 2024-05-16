0f5e066558d7e69f05bf213bbb29d8107d5d8a25
sponglebong <101961638+https433@users.noreply.github.com>
2024-05-16 08:45:41 -0700

    Update dynav.js
    𝄘 hamgburer

--- dynav.js
+++ dynav.js
@@ -34,7 +34,7 @@
             if (activeLink === 'blog') {
                 blogLink.addClass('active');
             }
-            const iconLink = $('<a>').attr('href', 'javascript:void(0);').css('fontSize', '15px').addClass('icon').click(displaynav);
+            const iconLink = $('<a>').attr('href', 'javascript:void(0);').css('fontSize', '15px').addClass('icon').text('𝄘').click(displaynav);
 
             projectsDropdown.append(projectsButton);
             projectsButton.append(projectsIcon);
